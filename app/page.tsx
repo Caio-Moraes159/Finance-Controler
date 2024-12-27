9b03de94c0b746ffbbd3cb47ed01f98ede7de157
Caio Caminitti <caiomoraes858@gmail.com>
2024-12-27 14:55:53 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex intens-center justify-center w-screen">
       <h1 className="text-red-500 p-5"> Page Home Teste</h1>
+      <Button>Wello Wolrd</Button>
     </div>
   );
 }
