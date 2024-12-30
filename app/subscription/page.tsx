a2cf5339ec5b8b00dcf6809860ce62cae7e6ac63
Caio Caminitti <caiomoraes858@gmail.com>
2024-12-30 16:35:12 -0300

    feat: ensure transaction and subscription pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,12 @@
+import { auth } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
+import { redirect } from "next/navigation";
 
-const subscription = () => {
+const subscription = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return <Navbar />;
 };
 
